20aec80130232f60c15d8124e6cba3a348a28a46
jess bacchi <135135190+cheecsey@users.noreply.github.com>
2025-10-26 10:06:06 -0400

    Money Counter Working

--- Unplanted-Crimes/Assets/Scripts/MoneyCountScript.cs
+++ Unplanted-Crimes/Assets/Scripts/MoneyCountScript.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MoneyCountScript : MonoBehaviour
 {
-    [SerializeField] Text moneyCounterText; // text box to display amount of money player has
+    [SerializeField] private TextMeshProUGUI moneyCounterText; // text box to display amount of money player has
     private int moneyCounter;
 
     void Start()
     {
         moneyCounter = 0; // auto set money to 0 at start of game
+
+        UpdateMoneyCountUI();
     }
 
     private void Update()
@@ -16,10 +19,12 @@ public class MoneyCountScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q)) // press 'q' to sell all your money
         {
             moneyCounter = 0; // sets the players money back to 0
+            UpdateMoneyCountUI();
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
             moneyCounter = moneyCounter + 10;
+            UpdateMoneyCountUI();
         }
     }
 
